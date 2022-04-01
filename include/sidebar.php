22147ca2bb9d36d7311abf5c3fae272633eeefc9
Nirakar Bogati <lmntrix.as619@gmail.com>
2022-04-01 13:33:05 +0545

    completed

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Project Management</title>
   
+  <title>Project Management</title>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
